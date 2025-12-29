7a3664ab8baa9c1842c216b1c632360a98b484cf
Vijay Misal <vijaymisal894@gmail.com>
2025-12-29 11:58:17 +0530

    Fix listing model, review controller, and search route

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,21 +1,22 @@
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
 });
 
+
 const storage = new CloudinaryStorage({
-  cloudinary,
+  cloudinary: cloudinary,
   params: {
-    folder: "wanderlust",
-    allowed_formats: ["jpeg", "png", "jpg"],
+    folder: 'wanderlust_DEV',
+    allowedFormats:["png","jpg","jpeg"],
   },
 });
 
 module.exports = {
-  cloudinary,
-  storage,
-};
+    cloudinary,
+    storage,
+}
